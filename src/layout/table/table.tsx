63d1250dc52f13bf704d5537bd8e1446ab533667
RahulDas-dev <r.das699@gmail.com>
2024-02-23 01:36:20 +0530

    Table Align ment Fixed

--- src/layout/table/table.tsx
+++ src/layout/table/table.tsx
@@ -1,40 +1,54 @@
 import { FunctionComponent, ReactElement, useContext, useEffect, useState   } from 'react'
 import { DataContext } from '../../context/data_context';
 import classNames from 'classnames';
+import './table.css';
 
 const DataTable: FunctionComponent = ():ReactElement =>{
     const [is_table_populated, setTableisPolulated] = useState<boolean>(true)
     const {dataframe} = useContext(DataContext)
 
-    const layout = {
-        header: {
-            align: "center",
-            line: {width: 1, color: 'rgb(50, 50, 50)'},
-            fill: {color: "pink"},
-            font: {family: "Arial", size: 12, color: "white"}
-        },
-        cells: {
-            align: "center",
-            line: {color: "black", width: 1},
-            font: {family: "Arial", size: 50, color: ["black"]}
-        }  
-    }
-
+    const headerStyle = {
+        align: "center",
+        fill: { color: ['lightgrey'] },
+        font: { family: "Arial", size: 10, color: "black" },
+        
+      }
+      const cellStyle = {
+        align: ["center"],
+        line: { color: "black", width: 0.1 }
+      }
 
     useEffect(()=>{
         if (dataframe.size > 0){
             setTableisPolulated(true)
             dataframe.plot('table_plot_area').table({
-               layout
-            })
+                config: {
+                  displaylogo: false,
+                  responsive: true,
+                  displayModeBar: false,
+                  tableHeaderStyle: headerStyle,
+                  tableCellStyle: cellStyle,
+                  sendData: false
+                },
+                layout: {
+                  // title: "Table displaying the Titanic dataset",
+                  automargin: true,
+                  // height: 200,
+                  autosize: true,
+                  margin: {t:0,r:0,b:0,l:0},
+                  paper_bgcolor: 'gray'
+                }
+              })
         }else{
             setTableisPolulated(false)
         }
     },[dataframe])
 
     return (
-        <div className='static-table'>
-            <div className={classNames("block",{'h-96': is_table_populated })} id='table_plot_area'></div>
+        <div className={classNames('static-table', 'p-5', {'h-96': is_table_populated,
+                                                    'dark:bg-neutral-700': is_table_populated,
+                                                    'bg-neutral-300': is_table_populated})}>
+            <div id='table_plot_area'></div>
         </div>
     )
 };
